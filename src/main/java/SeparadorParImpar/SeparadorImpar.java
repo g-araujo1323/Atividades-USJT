5dc5f63062a065be4615911ccb4cba1511fdacad
Gabriel Araujo <gaa1323@gmail.com>
2022-09-12 16:21:28 -0300

    subindo separaador par e impar

---
+++ src/main/java/SeparadorParImpar/SeparadorImpar.java
@@ -0,0 +1,20 @@
+
+package SeparadorParImpar;
+
+public class SeparadorImpar {
+    
+    public static void main (String[] args) {
+        
+        for (int separador = 1; separador <=50; separador++) {
+        if (separador%2!=0) {
+            
+            System.out.println ("Resultado com números ímpares: " +separador);
+           }   
+              
+        }
+        
+        
+    }
+    
+   
+}
---
+++ src/main/java/SeparadorParImpar/SeparadorPar.java
@@ -0,0 +1,21 @@
+
+package SeparadorParImpar;
+
+public class SeparadorPar {
+    
+    public static void main (String[] args) {
+        
+        int separador =1;
+        while (separador <=50) {
+            
+            if ( separador % 2 == 0 ) {
+		System.out.println ( "Resultado com números pares: " + separador );}
+		separador ++;
+                
+	}
+            
+        }
+        
+    }
+    
+
